d3cb3027176a8640937888d69705e2c815440a8f
wangcheng12 <wangcheng12@xiaomi.com>
2020-08-04 10:21:52 +0800

    JVM Descriptors summary...

---
+++ ClassFileFormat/src/descriptors/FieldDescriptions.java
@@ -0,0 +1,22 @@
+package descriptors;
+
+/**
+ * @author bertking
+ * @Package jvm
+ * @Description: someLearn
+ * @date 2020/8/3-4:51 下午
+ */
+public class FieldDescriptions {
+    public byte mByte = 'A';
+    public char mChar = 'A';
+    public short mShort = 1;
+    public int mInt = 1;
+    public boolean mBoolean = true;
+    public long mLong = 10L;
+    public float mFloat = 3.14F;
+    public double mDouble = 3.1415926;
+    public String name = "Bertking";
+    public int[] mOneDimensionIntArray= new int[5];
+    public int[][] mTwoDimensionIntArray= new int[3][3];
+
+}
---
+++ ClassFileFormat/src/descriptors/MethodDescriptions.java
@@ -0,0 +1,21 @@
+package descriptors;
+
+/**
+ * @author bertking
+ * @Package jvm
+ * @Description: someLearn
+ * @date 2020/8/3-5:26 下午
+ */
+public class MethodDescriptions {
+
+    public MethodDescriptions() {
+    }
+
+   public void showTime(){
+
+   }
+
+   public String getName(String name,boolean gender,int age){
+        return "Bertking";
+   }
+}
